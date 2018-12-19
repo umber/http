0fa68e6be4813a7768083820382478f2de8fe1e0
Matthew Usurp <matt.usurp@gmail.com>
2018-12-19 14:10:44 +0000

    Removed dependency on `umber/common` for the exceptions.

--- src/Umber/Http/Exception/Response/ResponseMissingHeaderException.php
+++ src/Umber/Http/Exception/Response/ResponseMissingHeaderException.php
@@ -4,32 +4,26 @@ declare(strict_types=1);
 
 namespace Umber\Http\Exception\Response;
 
-use Umber\Common\Exception\AbstractMessageRuntimeException;
+use RuntimeException;
 
 /**
  * An exception thrown when a response does not have a http header when requested.
  */
-final class ResponseMissingHeaderException extends AbstractMessageRuntimeException
+final class ResponseMissingHeaderException extends RuntimeException
 {
     /**
      * @return ResponseMissingHeaderException
      */
     public static function create(string $name): self
     {
-        return new self([
-            'name' => $name,
-        ]);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public static function message(): array
-    {
-        return [
+        $message = implode(' ', [
             'The current response does not have the header by name "{{name}}".',
             'Please make sure that a response was set with this header before hand.',
             'To check for header by name make use of the "hasHeader()" method against the response.',
-        ];
+        ]);
+
+        return new self(strtr($message, [
+            '{{name}}' => $name,
+        ]));
     }
 }
--- src/Umber/Http/Exception/Response/ResponseMissingPaginatorException.php
+++ src/Umber/Http/Exception/Response/ResponseMissingPaginatorException.php
@@ -4,30 +4,24 @@ declare(strict_types=1);
 
 namespace Umber\Http\Exception\Response;
 
-use Umber\Common\Exception\AbstractMessageRuntimeException;
+use RuntimeException;
 
 /**
  * An exception thrown when a response does not have a pagination when requested.
  */
-final class ResponseMissingPaginatorException extends AbstractMessageRuntimeException
+final class ResponseMissingPaginatorException extends RuntimeException
 {
     /**
      * @return ResponseMissingPaginatorException
      */
     public static function create(): self
     {
-        return new self([]);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public static function message(): array
-    {
-        return [
+        $message = implode(' ', [
             'The current response does not have a paginator.',
             'Please make sure that a paginator was set before hand.',
             'To check for paginator make use of the "hasPaginator()" method against the response.',
-        ];
+        ]);
+
+        return new self($message);
     }
 }
